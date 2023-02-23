575272cf831d92053516b00d81e6ae3531abf94f
Rushil Borad <rushilborad@gmail.com>
2023-02-23 19:30:07 +0530

    Major commits

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+import mealsImage from '../../assets/meals.jpg';  
+import classes from './Header.module.css';
+import HeaderCardButton from "./HeaderCardButton";
+
+const Header = props => {
+
+    return <Fragment>
+         <header className={classes.header}>
+            <h1>React Meals</h1>
+            <HeaderCardButton />
+         </header>
+         <div className={classes['main-image']}>
+            <img src={mealsImage} alt="A meals photo"/>
+         </div>
+    </Fragment>
+};
+
+export default Header;
