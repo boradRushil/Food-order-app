3c31fb781b4ffc9f2bfd6c02b06b6fa39094c71b
Rushil Borad <rushilborad@gmail.com>
2023-03-20 12:20:49 +0530

    Connected to firebase

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,34 +1,37 @@
-import { useContext } from "react";
-import classes from "./MealItem.module.css";
-import MealItemForm from "./MealItemForm";
-import CartContext from "../../../store/cart-context";
+import { useContext } from 'react';
 
+import MealItemForm from './MealItemForm';
+import classes from './MealItem.module.css';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
-  
-  const cartCtx= useContext(CartContext);
-  const price = `$${props.price.toFixed(2)}`;
+  const cartCtx = useContext(CartContext);
 
-  const addToCartHandler = amount=> {
-      cartCtx.addItem({
-        id: props.id,
-        mane: props.name,
-        amount: amount,
-        price: props.price
-      });
+  // console.log(typeof props.price)
+
+  const price = `$${Number(props.price).toFixed(2)}`;
+
+  const addToCartHandler = amount => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price
+    });
   };
+
   return (
     <li className={classes.meal}>
       <div>
-        <div>
-          <h3>{props.name}</h3>
-        </div>
+        <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
- 
-      <MealItemForm onAddToCart={addToCartHandler}/>
+      <div>
+        <MealItemForm onAddToCart={addToCartHandler} />
+      </div>
     </li>
   );
 };
+
 export default MealItem;
